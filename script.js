3104abe3ad1535081e089204b62f60359a200ee7
mMazurkiewicz <mazurkorn@gmail.com>
2021-08-05 17:01:53 +0200

    sticky, accordion, footer

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   const dt = new Date();
-  document.getElementById("currentYear").innerHTML=dt.getFullYear();
+  document.getElementById("currentYear").innerHTML = dt.getFullYear();
+
+  const row = document.getElementsByClassName("expandalbe")[0];
+  const accordion = document.getElementsByClassName("accordion")[0];
+  const expandButton = document.getElementsByClassName("expandButton")[0];
+  
+  row.addEventListener('click', (e) => {
+    expandButton.classList.toggle("show");
+    expandButton.classList.contains("show") 
+      ? expandButton.innerHTML = 'remove' 
+      : expandButton.innerHTML ='add';
+
+    if (accordion.style.maxHeight) {
+      accordion.style.maxHeight = null;
+    } else {
+      accordion.style.maxHeight = '90px';
+    }
+  })
 })
